6b7eba7e098da426c8d3f53e0d3956704934a0d1
Rayan LALAOUI <rayan.lalaoui.768@gmail.com>
2023-02-10 17:45:03 +0100

    Ajout de la fonction qui determine la postions des murs et celle qui dessine les noeuds (supposé dans Sage)

--- src/affichage.py
+++ src/affichage.py
@@ -1,4 +1,45 @@
+from utilites import *
+
+
 '''
 Module contenant les différents méthodes d'affichage de labyrinthes.
 '''
 
+directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+cote = ["haut", "droite", "gauche", "bas"]
+
+
+def positionsMurs(n: Noeud) -> list:
+    connexions = n.get_connexions()
+    vectors = [(node[0]-n.get_id()[0], node[1]-n.get_id()[1])
+               for node in connexions]
+    murs = []
+    for v in directions:
+        if v not in vectors:
+            # pour un affichage verbeux switch directions pour cote avant []
+            murs.append(directions[directions.index(v)])
+    return murs
+
+
+# code probablement orienté sage, on verra
+def dessineNoeud(n: Noeud) -> None:
+    murs = positionsMurs(n)
+    lines = []
+    for m in murs:
+        if m == (-1, 0):
+            lines.append([n.get_id(), (n.get_id()[0], n.get_id()[1]+1)])
+        elif m == (1, 0):
+            lines.append([(n.get_id()[0]+1, n.get_id()[1]),
+                         (n.get_id()[0]+1, n.get_id()[1]+1)])
+        elif m == (0, 1):
+            lines.append([(n.get_id()[0], n.get_id()[1]+1),
+                         (n.get_id()[0]+1, n.get_id()[1]+1)])
+        elif m == (0, -1):
+            lines.append([n.get_id(), (n.get_id()[0]+1, n.get_id()[1]+1)])
+
+    for line in lines:
+        line2d(line)
+
+
+n = Noeud((3, 4), [(4, 4), (3, 3), (3, 5)])
+print(positionsMurs(n))
