3a633ad1622f78a2863df98e4108f2d28c427ae6
Alex Scofield <alexscofieldt@gmail.com>
2023-02-15 09:32:37 +0100

    Connexions et vérifications testés et corrigés

--- test_utilites.py
+++ test_utilites.py
@@ -14,11 +14,39 @@ class TestInitialisationPseudoLabyrinthe(unittest.TestCase):
         for noeud in pseudo_labyrinthe.get_noeuds():
             self.assertTrue(noeud.get_connexions()==[])
 
+class TestGetNoeudParId(unittest.TestCase):
+    def setUp(self) -> None:
+        self.pseudo_labyrinthe = PseudoLabyrinthe((4,4))
+
+    def test_get_noeud_par_id(self):
+        noeud = self.pseudo_labyrinthe.get_noeud_par_id((2,3))
+        self.assertEqual(noeud.get_id(),(2,3))
+
+    def test_get_noeud_par_id_erreur(self):
+        self.assertRaises(ValueError, self.pseudo_labyrinthe.get_noeud_par_id, (4,2))
+
 class TestVoisins(unittest.TestCase):
-    def test_get_voisins(self):
+    def test_get_voisins_est_list(self):
         pseudo_labyrinthe = PseudoLabyrinthe(TAILLE_TEST)
         noeud_premier: Noeud = pseudo_labyrinthe.get_noeud_par_id((0,0))
         self.assertTrue(type(noeud_premier.get_voisins(pseudo_labyrinthe))==list)
+    
+    def test_get_voisins_tailles(self):
+        pseudo_labyrinthe = PseudoLabyrinthe(TAILLE_TEST)
+        for noeud in pseudo_labyrinthe.get_noeuds():
+            self.assertTrue(len(noeud.get_voisins(pseudo_labyrinthe))<=4)
+    
+    def test_get_voisins_normal(self):
+        pseudo_labyrinthe = PseudoLabyrinthe((4,4))
+        noeud_test = pseudo_labyrinthe.get_noeud_par_id((2,2))
+        voisins_id = [voisin.get_id() for voisin in noeud_test.get_voisins(pseudo_labyrinthe)]
+        self.assertTrue(set(voisins_id)=={(1,2),(3,2), (2,1),(2,3)})
+    
+    def test_get_voisins_cas_extreme(self):
+        pseudo_labyrinthe = PseudoLabyrinthe((3,2))
+        noeud_test = pseudo_labyrinthe.get_noeud_par_id((2,1))
+        voisins_id = [voisin.get_id() for voisin in noeud_test.get_voisins(pseudo_labyrinthe)]
+        self.assertTrue(set(voisins_id)=={(1,1), (2,0)})
 
 class TestAjouteConnexions(unittest.TestCase):
     def test_ajoute_connexions_vide(self):
