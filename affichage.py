7863635635dbedd70ed8b32bdf77dfb55074ed8f
Rayan LALAOUI <rayan.lalaoui.768@gmail.com>
2023-02-16 14:28:51 +0100

    "

--- affichage.py
+++ affichage.py
@@ -3,27 +3,16 @@ Module contenant les différents méthodes d'affichage de labyrinthes.
 '''
 
 from utilites import *
+import algorithmes
+directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+cote = ["haut", "droite", "gauche", "bas"]
 
 
 directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
 cote = ["haut", "droite", "gauche", "bas"]
 
 
-def positionsMurs(n: Noeud) -> list:
-    connexions = n.get_connexions()
-    vectors = [(node[0]-n.get_id()[0], node[1]-n.get_id()[1])
-               for node in connexions]
-    murs = []
-    for v in directions:
-        if v not in vectors:
-            # pour un affichage verbeux switch directions pour cote avant []
-            murs.append(directions[directions.index(v)])
-    return murs
-
-
-# code probablement orienté sage, on verra
-def dessineNoeud(n: Noeud) -> None:
-    murs = positionsMurs(n)
+def dessineNoeud(n: Noeud, murs: list) -> None:
     lines = []
     for m in murs:
         if m == (-1, 0):
@@ -37,15 +26,23 @@ def dessineNoeud(n: Noeud) -> None:
         elif m == (0, -1):
             lines.append([n.get_id(), (n.get_id()[0]+1, n.get_id()[1])])
     return lines
-    
-        
-        
-def afficheLaby(L : PseudoLabyrinthe):
-    lines=[]
-    for n in L.get_noeuds():
-            lines.append(dessineNoeud(n))
 
 
-
-n = Noeud((3, 4), [(4, 4), (3, 3), (3, 5)])
-print(positionsMurs(n))
+def afficheLaby(pl: PseudoLabyrinthe) -> list:
+    plot = line2d([(0, 0), (0, 0)], xmin=0, xmax=pl.get_taille()[0], ymin=0, ymax=pl.get_taille()[1])
+    for n in pl.get_noeuds():
+        connexions = n.get_connexions()
+        nid = n.get_id()
+        vectors = [(node.get_id()[0]-nid[0], node.get_id()[1]-nid[1]) for node in connexions]
+        murs = []
+        for v in directions:
+            if v not in vectors:
+                murs.append(directions[directions.index(v)])
+        for m in murs:
+            if (0<=nid[0]+m[0]<pl.get_taille()[0] and 0<=nid[1]+m[1]<pl.get_taille()[1]):
+                if n in pl.get_noeud_par_id((nid[0]+m[0],nid[1]+m[1])).get_connexions():
+                    murs.remove(m)
+        lines = dessineNoeud(n, murs)
+        for line in lines:
+            plot += line2d(line)
+    return plot
